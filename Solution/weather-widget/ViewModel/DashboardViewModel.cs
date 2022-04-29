27053b37d14a44763627bbfcdca0ba51f97ad63b
botchGNU <kickers.world.tim@gmail.com>
2022-04-29 09:33:28 +0200

    implement listview

--- Solution/weather-widget/ViewModel/DashboardViewModel.cs
+++ Solution/weather-widget/ViewModel/DashboardViewModel.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Windows.Input;
 using weather_widget.Command;
+using weather_widget.Model;
 using weather_widget.Store;
 
 namespace weather_widget.ViewModel
 {
     class DashboardViewModel : ViewModelBase
     {
+        private WeatherInfoListModel _testingList = new WeatherInfoListModel();
+
+        private void FillTestingList()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var weatherNew = new WeatherInfoModel("Cloudy","N.A.",DateTime.Now,i+10,i,22,i+5,5);
+                _testingList.Add(weatherNew);
+            }
+            OnPropertyChanged(nameof(ForecastList));
+        }
+
+        #region ctor
         public DashboardViewModel(NavigationStore navigationStore, Func<SettingsViewModel> createSettingsViewModel)
         {
             SettingsButtonCommand = new NavigateCommand(navigationStore, createSettingsViewModel);
             CloseButtonCommand = new ExitApplicationCommand();
+            FillTestingList();
         }
-
-
+        #endregion
         #region commands
         public ICommand SettingsButtonCommand { get; } //Command in order to switch to Settings-View
         public ICommand CloseButtonCommand { get; } //Command in order to terminate entire application
@@ -22,9 +36,18 @@ namespace weather_widget.ViewModel
         #region properties
         public string CurrentDate { get => DateTime.Now.ToString();}
         public string CurrentDay { get => DateTime.Now.DayOfWeek.ToString(); }
-        public string CurrentLocation { get => "Entenhausen DE"; }
+        public string CurrentLocation 
+        { 
+            get => "Entenhausen DE"; 
+            set 
+            {
+                 // = value;
+                OnPropertyChanged(nameof(CurrentLocation));    
+            }
+        }
         public string CurrentTemperature { get => "22 C"; } //placeholder
         public string CurrentType { get => "Cloudy"; }  //placeholder
+        public WeatherInfoListModel ForecastList { get => _testingList; }
 
         #endregion
     }
