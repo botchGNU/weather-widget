4cd50b1bf4faa7086d8dcc314dd5c1a73bbad33c
mikailylk <mikail.yoelek@student.htl-rankweil.at>
2022-05-21 15:00:54 +0200

    Changes to not have merge errors

--- Solution/weather-widget/MainWindow.xaml.cs
+++ Solution/weather-widget/MainWindow.xaml.cs
@@ -29,74 +29,7 @@ namespace weather_widget
 
             InitializeComponent();
             weatherInfos = new WeatherInfoListModel();
-            /*
-            //List<WeatherInfoModel> weatherInfo = await APIManagerModel.GetWeather("Rankweil");
-
-            //Task<WeatherInfoListModel> weatherInfos = APIManagerModel.GetWeather("Rankweil");
-            /*
-            try
-            {
-                GetWeather("Rankweil");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-
-            Debug.WriteLine("Do Other stuff");
-            Debug.WriteLine("Do Other stuff");
-            Debug.WriteLine("Do Other stuff");
-            Debug.WriteLine("Do Other stuff");
-            */
-            /*
-            DataBaseManagerModel dataBaseManagerModel = new DataBaseManagerModel();
-            List<string> cities = dataBaseManagerModel.GetCitiesByLetters("Rankwe");
-            foreach (string item in cities)
-            {
-                Debug.WriteLine(item);
-            }
-
-
-            //dataBaseManagerModel.GetDataFromOpenWeather("Rankweil");
-            
-            dataBaseManagerModel.GetDataFromOpenWeather("London");
-
-            Debug.WriteLine("Do other stuff");
-            Debug.WriteLine("Do other stuff");
-            Debug.WriteLine("Do other stuff");
-            Debug.WriteLine("Do other stuff");
-            Debug.WriteLine("Do other stuff");
-            Debug.WriteLine("Do other stuff");
-            */
-            dataBaseManagerModel = new DataBaseManagerModel();
-            //dataBaseManagerModel.GetDataFromOpenWeather("Rankweil");
-
-            string s = "halleo";
         }
-
-        private void WindowLoaded(object sender, RoutedEventArgs e)
-        {
-            dataBaseManagerModel.LoadFromDatabase();
-        }
-
-
-        /*
-        private async void GetWeather(string cityname)
-        {
-            APIManagerModel apimanagerModel = new APIManagerModel();
-            Task<WeatherInfoListModel> TaskweatherInfos = apimanagerModel.GetWeather(cityname);
-
-            weatherInfos = await TaskweatherInfos;
-            Debug.WriteLine(weatherInfos.Count);
-
-            /*
-            Task<string> s = api.GetWeather("Rankweil");
-            string ss = await s;
-            Debug.WriteLine(ss);
-            
-        } 
-    */
     }
 
 
